712a52aa208b6b636c1c5946a5fa80168562ad90
Moises Zeleny <moiseszeleny@gmail.com>
2025-06-10 21:59:40 -0600

    Update file creation timestamps and modify gauge spinor definitions in DLRSM model files

--- SPheno-4.0.5/DLRSM/Model_Data_DLRSM.f90
+++ SPheno-4.0.5/DLRSM/Model_Data_DLRSM.f90
@@ -4,7 +4,7 @@
 !           1405.1434, 1411.0675, 1503.03098, 1703.09237, 1706.05372, 1805.07306  
 ! (c) Florian Staub, Mark Goodsell and Werner Porod 2020  
 ! ------------------------------------------------------------------------------  
-! File created at 13:48 on 10.6.2025   
+! File created at 21:43 on 10.6.2025   
 ! ----------------------------------------------------------------------  
  
  
@@ -1213,6 +1213,7 @@ Complex(dp) :: ratioPGG(4),ratioPPP(4)
 Real(dp) :: gForTadpoles(144)
 Complex(dp) :: tForTadpoles(2)
 Real(dp) :: gBL_saveEP 
+Real(dp) :: g2_saveEP 
 Real(dp) :: g2_saveEP 
 Complex(dp) :: Y_saveEP(3,3) 
 Complex(dp) :: YQ1_saveEP(3,3) 
